e54c60dce0e911bf87719974fcf4cb3c08291578
Lindsay loughlin <loughlil@loughlils-MacBook-Pro.local>
2021-02-08 17:56:58 +1100

    add new function

---
+++ common_data.go
@@ -0,0 +1,14 @@
+package common
+
+// GetCommonData a method
+func GetCommonData() string {
+	return "Common Data"
+}
+
+func GetData() string {
+	return "Data"
+}
+
+func NewVersionFunction() string {
+	return "New version function"
+}
